8f64e3340dac253c524070d425cd4b006ccacc7f
PNg-HA <21520586@gm.uit.edu.vn>
2025-05-17 22:14:37 +0700

    first

---
+++ python-server/workshop-setup.sh
@@ -0,0 +1,17 @@
+# This is only required by the instructor-led workshop
+#!/bin/bash
+
+# Start virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+
+# install dependencies
+pip install -r requirements.txt
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Set websocket server host and port
+export HOST="0.0.0.0"
+export WS_PORT=8081
